dcb85a6598cfaf976062e3305644b45e284cbf17
JoeyJiao <joey.jiaojg@gmail.com>
2011-11-10 22:18:36 +0800

    remove redundant sprintf from request_module call

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -409,21 +409,13 @@ static int cpufreq_parse_governor(char *str_governor, unsigned int *policy,
 		t = __find_governor(str_governor);
 
 		if (t == NULL) {
-			char *name = kasprintf(GFP_KERNEL, "cpufreq_%s",
-								str_governor);
+			int ret;
 
-			if (name) {
-				int ret;
-
-				mutex_unlock(&cpufreq_governor_mutex);
-				ret = request_module("%s", name);
-				mutex_lock(&cpufreq_governor_mutex);
-
-				if (ret == 0)
-					t = __find_governor(str_governor);
-			}
-
-			kfree(name);
+			mutex_unlock(&cpufreq_governor_mutex);
+			ret = request_module("%s", name);
+			mutex_lock(&cpufreq_governor_mutex);
+			if (ret == 0)
+				t = __find_governor(str_governor);
 		}
 
 		if (t != NULL) {
