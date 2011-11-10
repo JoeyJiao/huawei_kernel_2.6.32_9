b93a6230884f892681805022bb55278600bc2329
JoeyJiao <joey.jiaojg@gmail.com>
2011-11-10 22:07:09 +0800

    lib/sha1: use the git implementation of SHA-1, which cannot boot

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,2 +1,2 @@
 #ARCH=arm CROSS_COMPILE=~/android/src/c8600/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make V=1 zImage -j2
-ARCH=arm CROSS_COMPILE=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make zImage -j4
+ARCH=arm CROSS_COMPILE=~/android/src/cm2.3.7/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make zImage -j4
--- build-modules.sh
+++ build-modules.sh
@@ -1,1 +1,1 @@
-ARCH=arm CROSS_COMPILE=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make modules -j4
+ARCH=arm CROSS_COMPILE=~/android/src/cm2.3.7/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make modules -j4
